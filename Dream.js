b948b9ae6b1f9ad77d92b5ddcf0c77a0a55cb95a
raaaaaaaaaaaaaz <45065665+raaaaaaaaaaaaaz@users.noreply.github.com>
2018-11-15 22:10:20 +0100

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,12 @@
+﻿client.on('message', message => {//!- Flix |     Ráááááz  , $!#8811 
+        if (!nounou.includes(message.author.id)) return;//!- Flix |     Ráááááz  , $!#8811 
+  if (message.content === 'R') {//!- Flix |     Ráááááz  , $!#8811 
+   // let channel = guild.channels.find('name', '➥ AFK.');//!- Flix |     Ráááááz  , $!#8811 
+
+  let channel = client.channels.get('511744532992229377');//!- Flix |     Ráááááz  , $!#8811 
+
+  channel.join()//!- Flix |     Ráááááz  , $!#8811 
+  .then(connection => message.channel.send(`Done ☑ By: !- Flix |     Ráááááz  , $!#8811 `))//!- Flix |     Ráááááz  , $!#8811 
+  .catch(console.error);//!- Flix |     Ráááááz  , $!#8811 
+  }
+});
