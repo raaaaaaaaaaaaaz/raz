027fcaed1e1977c559e19353dbb0018b8257be42
raaaaaaaaaaaaaz <45065665+raaaaaaaaaaaaaz@users.noreply.github.com>
2018-11-16 18:25:21 +0100

    Update Dream.js

--- Dream.js
+++ Dream.js
@@ -1,77 +1,12 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////
-const Discord1 = require('discord.js');
-const Discord2 = require('discord.js');
-//////////////////////////////////////////////////////////////////////////////////////////////////
-const client1 = new Discord1.Client();
-const client2 = new Discord2.Client();
-//////////////////////////////////////////////////////////////////////////////////////////////////
-var ServerID = "510514272229392384";
-var ChannelID = "512374264389828624";
-var prefix = "R";
-console.log('Love.. Idle.');
-//////////////////////////////////////////////////////////////////////////////////////////////////
-client1.on('ready', () => {
-client1.user.setActivity('Love..', {type: "STREAMING",url: "http://www.twitch.tv/dangernumber17"});
+client.on('message', message => {//!- Flix |     Ráááááz  , $!#8811 
+        if (!nounou.includes(message.author.id)) return;//!- Flix |     Ráááááz  , $!#8811 
+  if (message.content === 'Ab') {//!- Flix |     Ráááááz  , $!#8811 
+   // let channel = guild.channels.find('name', '➥ AFK.');//!- Flix |     Ráááááz  , $!#8811 
+
+  let channel = client.channels.get('511744532992229377');//!- Flix |     Ráááááz  , $!#8811 
+
+  channel.join()//!- Flix |     Ráááááz  , $!#8811 
+  .then(connection => message.channel.send(`Ok By: !- Flix |     Ráááááz  , $!#8811 `))//!- Flix |     Ráááááz  , $!#8811 
+  .catch(console.error);//!- Flix |     Ráááááz  , $!#8811 
+  }
 });
-client2.on('ready', () => {
-client2.user.setActivity('Love..', {type: "STREAMING",url: "http://www.twitch.tv/dangernumber17"});
-});
-//////////////////////////////////////////////////////////////////////////////////////////////////
-client1.on("message", message => {
-      if (message.author.bot) return;
-      if(!message.channel.guild) return;
-        if(message.content.startsWith(R + "say")) {
-message.delete(3000);
-    if(message.author.id !== "463698464845594628") return message.reply('You are not allowed to use this command!').then(msg => msg.delete(3000));
-        let args = message.content.split(" ").slice(1);
-message.channel.send(args).then(s => {
-s.delete(3000);
-});
-}
-});
-client2.on("message", message => {
-      if (message.author.bot) return;
-      if(!message.channel.guild) return;
-        if(message.content.startsWith(R + "say")) {
-message.delete(3000);
-    if(message.author.id !== "463698464845594628") return message.reply('You are not allowed to use this command!').then(msg => msg.delete(3000));
-        let args = message.content.split(" ").slice(1);
-message.channel.send(args).then(s => {
-s.delete(3000);
-});
-}
-});
-//////////////////////////////////////////////////////////////////////////////////////////////////
-client1.on('ready', () => {
-client1.user.setActivity('Love..', {type: "STREAMING",url: "http://www.twitch.tv/dangernumber17"});
-});
-client2.on('ready', () => {
-client2.user.setActivity('Love..', {type: "STREAMING",url: "http://www.twitch.tv/dangernumber17"});
-});
-//////////////////////////////////////////////////////////////////////////////////////////////////
-client1.on("message", message => {
-          if (message.author.bot) return;
-      if(!message.channel.guild) return;
-        if(message.content.startsWith(R + "rep")) {
-message.delete(3000);
-    if(message.author.id !== "463698464845594628") return message.reply('You are not allowed to use this command!').then(msg => msg.delete(3000));
-message.channel.send('#rep <@!463698464845594628').then(s => {
-s.delete(3000);
-});
-message.channel.send('->rep <@!463698464845594628>').then(s => {
-s.delete(3000);
-});
-}
-});
-client2.on("message", message => {
-          if (message.author.bot) return;
-      if(!message.channel.guild) return;
-        if(message.content.startsWith(R + "rep")) {
-message.delete(3000);
-    if(message.author.id !== "463698464845594628") return message.reply('You are not allowed to use this command!').then(msg => msg.delete(3000));
-message.channel.send('#rep <@!463698464845594628>').then(s => {
-s.delete(3000);
-});
-//////////////////////////////////////////////////////////////////////////////////////////////////
-client1.login("NDk1MzA3NDEzMDc5OTgyMDky.DsiCWw.pe6taJKFCEoyq4toUhjRGal2MDs");
-client2.login("NTEyNzY3MzgzOTEyNDQ4MDE1.Ds-Q0w.Age-vh6ycZUr1DGiR_pzGAFHKrY");
